66ba8093ac2b3ba570bb498a3dcde37f3349f656
Redox <mail@redox.to>
2022-09-19 15:38:48 +0200

    Remove old search bar WIP

--- lua/autorun/client/toolsearch.lua
+++ lua/autorun/client/toolsearch.lua
@@ -2,7 +2,6 @@ local cl_toolsearch_autoselect = CreateClientConVar( "cl_toolsearch_autoselect",
 local cl_toolsearch_favoritesonly = CreateClientConVar( "cl_toolsearch_favoritesonly", "0" )
 local cl_toolsearch_favoritestyle = CreateClientConVar( "cl_toolsearch_favoritestyle", "1" )
 local favorites = util.JSONToTable( file.Read( "tools_favorites.txt", "DATA" ) or "{}" ) or {}
-local init = false
 
 hook.Add( "PostReloadToolsMenu", "ToolSearch", function()
     local toolPanel = g_SpawnMenu.ToolMenu.ToolPanels[1]
@@ -18,95 +17,12 @@ hook.Add( "PostReloadToolsMenu", "ToolSearch", function()
     local panel = vgui.Create( "EditablePanel", divider )
     list:SetParent( panel )
     list:Dock( FILL )
-    local textEntry = panel:Add( "EditablePanel" )
-    textEntry:Dock( TOP )
-    textEntry:DockMargin( 0, 0, 0, 2 )
-    textEntry:SetTall( 20 )
-    local search = textEntry:Add( "DTextEntry" )
-    search:Dock( FILL )
-    search:DockMargin( 0, 0, 2, 0 )
-    search._Paint = search.Paint
-
-    function search:Paint( w, h )
-        local ret = self:_Paint( w, h )
-
-        if self:GetValue() == "" then
-            surface.SetFont( "DermaDefault" )
-            local txt = "Search Tool..."
-            local _, txtH = surface.GetTextSize( txt )
-            surface.SetTextPos( 3, h * 0.5 - txtH * 0.5 + 1 )
-            surface.SetTextColor( Color( 0, 0, 0, 192 ) )
-            surface.DrawText( txt )
-        end
-
-        return ret
-    end
-
-    search:SetUpdateOnType( true )
-
-    function search:OnValueChange( str, value )
-        local i = 0
-
-        for _, cat in next, list.pnlCanvas:GetChildren() do
-            local hidden = 0
-
-            for _, pnl in next, cat:GetChildren() do
-                if pnl.ClassName ~= "DCategoryHeader" then
-                    if language.GetPhrase( pnl:GetText() ):lower():match( str:lower() ) and ( not cl_toolsearch_favoritesonly:GetBool() or cl_toolsearch_favoritesonly:GetBool() and favorites[pnl.Name] ) then
-                        pnl:SetVisible( true )
-
-                        if cl_toolsearch_autoselect:GetBool() and not value then
-                            i = i + 1
-
-                            if i == 1 then
-                                pnl:SetSelected( true )
-                                pnl:DoClick()
-                            else
-                                pnl:SetSelected( false )
-                            end
-                        end
-                    else
-                        pnl:SetVisible( false )
-                        hidden = hidden + 1
-                    end
-                end
-            end
-
-            if hidden >= #cat:GetChildren() - 1 then
-                cat:SetVisible( false )
-            else
-                cat:SetVisible( true )
-            end
-
-            cat:InvalidateLayout()
-            list.pnlCanvas:InvalidateLayout()
-        end
-    end
-
-    local clear = textEntry:Add( "DButton" )
-    clear:Dock( RIGHT )
-    clear:SetWide( 20 )
-    clear:SetText( "" )
-    clear:SetTooltip( "Press to clear" )
-
-    function clear:DoClick()
-        search:SetValue( "" )
-    end
-
-    local close = Material( "icon16/cross.png" )
-
-    function clear:Paint( w, h )
-        derma.SkinHook( "Paint", "Button", self, w, h )
-        surface.SetMaterial( close )
-        surface.SetDrawColor( Color( 255, 255, 255 ) )
-        surface.DrawTexturedRect( w * 0.5 - 16 * 0.5, h * 0.5 - 16 * 0.5, 16, 16 )
-    end
 
     local favsOnly = panel:Add( "EditablePanel" )
     favsOnly:Dock( TOP )
     favsOnly:DockMargin( 0, 0, 0, 2 )
     favsOnly:SetTall( 20 )
-    local check = favsOnly:Add( "DCheckBoxLabel" )
+    local check = divider:Add( "DCheckBoxLabel" )
     check:SetConVar( "cl_toolsearch_favoritesonly" )
     local showFavsOnly = cl_toolsearch_favoritesonly:GetBool()
     check:SetChecked( showFavsOnly )
@@ -168,7 +84,43 @@ hook.Add( "PostReloadToolsMenu", "ToolSearch", function()
             end
         end
 
-        search:OnValueChange( search:GetValue(), not init or not showFavs )
+        local i = 0
+
+        for _, cat in next, list.pnlCanvas:GetChildren() do
+            local hidden = 0
+
+            for _, pnl in next, cat:GetChildren() do
+                if pnl.ClassName ~= "DCategoryHeader" then
+                    if not cl_toolsearch_favoritesonly:GetBool() or cl_toolsearch_favoritesonly:GetBool() and favorites[pnl.Name] then
+                        pnl:SetVisible( true )
+
+                        if cl_toolsearch_autoselect:GetBool() and not value then
+                            i = i + 1
+
+                            if i == 1 then
+                                pnl:SetSelected( true )
+                                pnl:DoClick()
+                            else
+                                pnl:SetSelected( false )
+                            end
+                        end
+                    else
+                        pnl:SetVisible( false )
+                        hidden = hidden + 1
+                    end
+                end
+            end
+
+            if hidden >= #cat:GetChildren() - 1 then
+                cat:SetVisible( false )
+            else
+                cat:SetVisible( true )
+            end
+
+            cat:InvalidateLayout()
+            list.pnlCanvas:InvalidateLayout()
+        end
+
         init = true
     end
 
